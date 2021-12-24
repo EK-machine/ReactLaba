74259a236d6535924447799539b27262ce860fb4
Yauhen Kalabukhin <yauhen.kalabukhin@itechart-group.com>
2021-12-24 12:52:06 +0300

    styling of dropdown

--- src/components/homePage/category.spec.js
+++ src/components/homePage/category.spec.js
@@ -1,17 +1,31 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable no-undef */
 import React from "react";
-// import { shallow } from "enzyme";
 import Category from "./category";
 
-// eslint-disable-next-line no-undef, react/jsx-filename-extension
-const setUp = () => shallow(<Category title="hello" icon="hi" path="/products/pc" />);
+const props = {
+  title: "hello",
+  icon: "hi",
+  path: "/products/pc",
+};
 
 describe("Category component tests:", () => {
-  let component;
-  beforeEach(() => {
-    component = setUp();
+  it("- Snapshot test", () => {
+    const component = shallow(<Category {...props} />);
+    expect(component).toMatchSnapshot();
   });
 
-  it("- Snapshot test", () => {
-    expect(component).toMatchSnapshot();
+  describe("- Has prop title", () => {
+    it("- Should render title", () => {
+      const component = shallow(<Category {...props} />);
+      const title = component.find(".category__title");
+      expect(title.length).toBe(1);
+    });
+
+    it("- Expected output of title shall be 'hello'", () => {
+      const component = shallow(<Category {...props} />);
+      const title = component.find(".category__title");
+      expect(title.text()).toBe("hello");
+    });
   });
 });
