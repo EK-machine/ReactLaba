2a83334caa5049691cf0706d2f7c9f96f0f95d97
Yauhen Kalabukhin <yauhen.kalabukhin@itechart-group.com>
2021-12-24 11:38:35 +0300

    pre-commit test

--- src/components/homePage/category.spec.js
+++ src/components/homePage/category.spec.js
@@ -5,23 +5,13 @@ import Category from "./category";
 // eslint-disable-next-line no-undef, react/jsx-filename-extension
 const setUp = () => shallow(<Category title="hello" icon="hi" path="/products/pc" />);
 
-describe("should be rendered in category component", () => {
+describe("Category component tests:", () => {
   let component;
   beforeEach(() => {
     component = setUp();
   });
 
-  it("should contain .category-container wrapper", () => {
-    const wrapper = component.find(".category__container");
-    expect(wrapper.length).toBe(1);
-  });
-
-  it("should contain div tag", () => {
-    const wrapper = component.find("div");
-    expect(wrapper.length).toBe(3);
-  });
-
-  it("is first snapshot test", () => {
+  it("- Snapshot test", () => {
     expect(component).toMatchSnapshot();
   });
 });
