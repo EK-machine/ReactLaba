134d36956f551bc81cc3dc51b0710b241795c4a9
Yauhen Kalabukhin <yauhen.kalabukhin@itechart-group.com>
2022-01-04 11:54:37 +0300

    added clarification to about page

--- src/components/aboutPage/aboutPage.tsx
+++ src/components/aboutPage/aboutPage.tsx
@@ -5,7 +5,8 @@ import CounterForErrorCatch from "../counterForErrorCatch";
 const AboutPage: React.FC = () => (
   <div className="about__container">
     <p>About Page</p>
-    <p>For now about page is empty and is used to test error boundary functionality using button below.</p>
+    <p>For now about page is empty and is used to test error boundary functionality using button below</p>
+    <p>(realized for development purposes only).</p>
     <CounterForErrorCatch />
   </div>
 );
