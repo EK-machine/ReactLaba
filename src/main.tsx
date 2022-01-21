e6a6164deb59ec1465f65043105c4cdfc88c0d5a
Yauhen Kalabukhin <yauhen.kalabukhin@itechart-group.com>
2022-01-21 08:49:24 +0300

    before scss restyling

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,4 @@
-import "./styles/main.css";
 import "./styles/main.scss";
-// watch: native intellisense and file-peek for aliases from jsconfig.json and with none-js files doesn't work: https://github.com/microsoft/TypeScript/issues/29334
 import React, { Component, StrictMode } from "react";
 import { Provider } from "react-redux";
 import ReactDom from "react-dom";
